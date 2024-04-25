224d4bf7cac9363b4074d6b5e93b2884be08f427
Yura-Radovets <158506814+Yura-Radovets@users.noreply.github.com>
2024-04-26 00:42:55 +0300

    add js

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const makeTransaction = `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+    return makeTransaction;
+}
+
+console.log(makeTransaction);
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
+
+
+
+
---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function getElementWidth(content, padding, border) {
+    let getElementWidth = Number.parseFloat(border) 
+    
+    return getElementWidth
+}
+
+console.log(getElementWidth("50", "8", "4px"));
+console.log(getElementWidth("60", "12", "8.5px"));
+// console.log(getElementWidth);
+// console.log(getElementWidth("50px", "8px", "4px"));
+// console.log(getElementWidth("60px", "12px", "8.5px"));
+// console.log(getElementWidth("200px", "0px", "0px"));
+
+// let value = "24.5px";
+// let numerical = Number.parseFloat(value);
+// console.log(numerical);
