535af19c688547daad672f1b3676bfd8d612134a
Yura-Radovets <158506814+Yura-Radovets@users.noreply.github.com>
2024-04-26 08:00:51 +0300

    modificate task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,16 +1,9 @@
 function getElementWidth(content, padding, border) {
-    let getElementWidth = Number.parseFloat(border) 
     
-    return getElementWidth
+    return Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2
 }
 
-console.log(getElementWidth("50", "8", "4px"));
-console.log(getElementWidth("60", "12", "8.5px"));
-// console.log(getElementWidth);
-// console.log(getElementWidth("50px", "8px", "4px"));
-// console.log(getElementWidth("60px", "12px", "8.5px"));
-// console.log(getElementWidth("200px", "0px", "0px"));
-
-// let value = "24.5px";
-// let numerical = Number.parseFloat(value);
-// console.log(numerical);
+console.log(getElementWidth);
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
